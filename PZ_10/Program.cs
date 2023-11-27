da652b8f17d552ccdfd8daad6e4eec6b603213de
Дарья Павловна Ахметова <akhmetova.dp2204@oksei.onmicrosoft.com>
2023-11-28 04:07:24 +0500

    добавлена 10 практика

--- PZ_10/Program.cs
+++ PZ_10/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите текст содержащий адрес электронной почты: ");
+            string text = Console.ReadLine();
+            int index = text.IndexOf("@"); //определяю индекс @
+
+            if (index != -1)
+            {
+                text = text.Remove(index); //удаляю все что после @
+                string un = text.Substring(0, index).Trim(); //удаляю пробелы до и после строки и создаю подстроку от 1 символа до @
+                int space = un.LastIndexOf(' '); // определяю индекс последнего пробела 
+                text = text.Remove(0, space); //удаляю все c первого символа до последнего пробела
+                Console.WriteLine($"Здравствуй, {text}!");
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно");
+            }
+
         }
     }
 }
