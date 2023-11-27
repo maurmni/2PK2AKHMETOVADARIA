757ac03a268737041ca60f7f2a0f7650cecc72ca
Дарья Павловна Ахметова <akhmetova.dp2204@oksei.onmicrosoft.com>
2023-11-28 03:57:22 +0500

    добавлена 9 пркатика

--- PZ_09new/Program.cs
+++ PZ_09new/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите текст: ");
+            {
+                int count = 1; string lastStr = null;      // бесконечный цикл для ввода строк пользователем
+                while (true)
+                {
+                    string userInput = Console.ReadLine(); 
+                    
+                    if (userInput.StartsWith("_"))        //если строка начинается с _ то выполняется условие 
+                    {
+                        lastStr = userInput;
+                        break;
+                    }
+                    count++;               // увеличение счетчика при каждом вводе строки                }
+                    Console.WriteLine("\n"); for (int i = 0; i < count; i++)
+                    {
+                        Console.WriteLine(lastStr);
+                    }
+                }
+            }
         }
     }
 }
