ab5f47157465ded098b81f264521caa775878804
Дарья Павловна Ахметова <akhmetova.dp2204@oksei.onmicrosoft.com>
2023-12-04 10:59:02 +0500

    добавлена 11 практика

--- PZ_11n/Program.cs
+++ PZ_11n/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите простое число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int summ;
+            int plus = GetLimitSumm(n, out summ);
+            Console.WriteLine($"Сумма простых чисел до {n}: {plus}");
+        }
+        static bool IsSimple(int x) //метод для определения простое число или нет
+        {
+            for (int i = 2; i < x; i++)
+            {
+                if (x % i == 0)
+                    return false;
+            }
+            return x > 1;
+        }
+        static int GetLimitSumm(int n, out int summ)  //метод для вычисления суммы простых чисел
+        {
+            summ = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                if (IsSimple(i))
+                {
+                    summ += i;
+                }
+            }
+            return (summ);
         }
     }
 }
