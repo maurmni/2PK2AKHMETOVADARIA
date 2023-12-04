06ce90b4b56d4e5321c887e3b8b4cfce1b059759
Дарья Павловна Ахметова <akhmetova.dp2204@oksei.onmicrosoft.com>
2023-12-04 11:16:34 +0500

    ДОБАВЛЕНА 14 ПРАКТИКА

--- PZ_14/Program.cs
+++ PZ_14/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace PZ_14
+﻿using System;
+
+namespace PZ_14
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*В файле input.txt записаны числа (каждое — с новой строки), их количество не превышает 
+100. Необходимо отсортировать их по возрастанию и записать в файл output.txt*/
+
+            Console.WriteLine("Cоздание файла input и output");
+            File.Create("C:\\input.txt");
+            string input = "C:\\input.txt";
+            string output = "C:\\output.txt";
+            using (StreamWriter writer = new StreamWriter(input))
+            {
+                Random rnd = new Random();
+                for (int random = 0; random < 100; random++)
+                {
+                    writer.WriteLine(rnd.Next());
+                }
+            }
+            Console.WriteLine("Запись случайный чисел в файл input.txt завершена");
+            using (StreamReader reader = new StreamReader(input))
+            {
+                string line;
+                int[] numbers = new int[100];
+                int index = 0;
+                Array.Sort(numbers);
+
+                using (StreamWriter outputWriter = new StreamWriter(output))
+                {
+                    foreach (int num in numbers)
+                    {
+                        outputWriter.WriteLine(num);
+                    }
+                    Console.WriteLine("Сортировка и запись чисел в файл output.txt завершенa");
+                }
+            }
         }
     }
 }
