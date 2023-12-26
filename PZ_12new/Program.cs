381322272805b280e8cfa0dab5e96d3c5cd0cee8
Дарья Павловна Ахметова <akhmetova.dp2204@oksei.onmicrosoft.com>
2023-12-26 12:38:40 +0500

    добавлена 12 практика

--- PZ_12new/Program.cs
+++ PZ_12new/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace PZ_12new
+﻿using System;
+
+namespace PZ_12new
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("введите строку: ");
+            string str = Console.ReadLine();
+            string strReverse = Rever(str);
+            Console.WriteLine($"Перевернутая строка: {strReverse}");
+        }
+
+        static string Rever(string str)
+        {
+            char[] ch = str.ToCharArray();
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                char temp = ch[i];
+                ch[i] = ch[str.Length - i - 1];
+                ch[str.Length - i - 1] = temp;
+            }
+            return new string(ch);
         }
     }
 }
