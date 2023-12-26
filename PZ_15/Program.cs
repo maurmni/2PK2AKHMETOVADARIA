f35472a3304fd285d68cc14601130ef77ca5c97b
Дарья Павловна Ахметова <akhmetova.dp2204@oksei.onmicrosoft.com>
2023-12-26 12:36:10 +0500

    добавлена 15 практика

--- PZ_15/Program.cs
+++ PZ_15/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите полный путь к каталогу: "); string link = Console.ReadLine();
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(link);
+                if (directory.Exists)
+                {
+                    FileInfo[] files = directory.GetFiles();
+                    foreach (FileInfo file in files)
+                    {
+                        if (file.Length >= 10485761) // 10 Мб = 10485760 байт
+
+                          Console.WriteLine($"Файл размером больше 10 Мб: {file.Name}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Путь указан неправильно");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка:");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
