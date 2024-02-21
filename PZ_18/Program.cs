8be5eac2208a6f9b4aab4d04ebb079f8b6a284ce
Дарья Павловна Ахметова <akhmetova.dp2204@oksei.onmicrosoft.com>
2024-02-21 10:07:08 +0500

    добавлена 18 практика

---
+++ PZ_18/Program.cs
@@ -0,0 +1,26 @@
+﻿using static PZ_18.ticketInfo;
+
+namespace PZ_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AirplaneTicket ticket = new AirplaneTicket();
+
+            ticket.SeatNumber = "A58";
+            ticket.DepartureTime = DateTime.Now.AddHours(24);
+            ticket.PassengerName = "Павел Павлов";
+            ticket.Category = ticketCategory.Economy; 
+
+            Console.WriteLine("Информация о билете:");
+            Console.WriteLine($"Номер места: {ticket.SeatNumber}");
+            Console.WriteLine($"Дата/время вылета: {ticket.DepartureTime}");
+            Console.WriteLine($"ФИО пассажира: {ticket.PassengerName}");
+            Console.WriteLine($"Категория билета: {ticket.Category}");
+
+            ticket.DisplayRemainingTimeUntilDeparture();
+        }
+
+    }
+}
---
+++ PZ_18/ticketInfo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PZ_18.ticketInfo;
+
+namespace PZ_18
+{
+    internal class ticketInfo
+    {
+        public enum ticketCategory { Economy, Business }
+
+        public class AirplaneTicket
+        {
+            public ticketCategory Category { get; set; }
+            private string seatNumber;
+            private string passengerName;
+            public string SeatNumber
+            {
+                get { return seatNumber; }
+                set
+                {
+                    if (!string.IsNullOrWhiteSpace(value))
+                    {
+                        seatNumber = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите номер места.");
+                    }
+                }
+            }
+            public string PassengerName
+            {
+                get { return passengerName; }
+                set
+                {
+                    if (!string.IsNullOrWhiteSpace(value))
+                    {
+                        passengerName = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите ФИО пассажира.");
+                    }
+                }
+            }
+
+            public DateTime DateOfBirth { get; set; }
+            public DateTime DepartureTime { get; set; }
+
+            public decimal TicketPrice
+            {
+                get
+                {
+                    decimal price = (Category == ticketCategory.Business) ? 10000 : 7000;
+                    if ((DateTime.Today - DateOfBirth).TotalDays / 365 < 10)
+                    {
+                        price /= 2; 
+                    }
+                    return price;
+                }
+            }
+
+            public static int EconomyTicketsSold { get; set; }
+            public static int BusinessTicketsSold { get; set; }
+
+            public static void DisplaySoldTicketsCount()
+            {
+                Console.WriteLine($"Продано билетов в эконом классе: {EconomyTicketsSold}");
+                Console.WriteLine($"Продано билетов в бизнес классе: {BusinessTicketsSold}");
+            }
+
+            public void DisplayTicketInfo()
+            {
+                Console.WriteLine($"Номер места: {SeatNumber}");
+                Console.WriteLine($"Дата/время вылета: {DepartureTime}");
+                Console.WriteLine($"ФИО пассажира: {PassengerName}");
+                Console.WriteLine($"Категория билета: {Category}");
+            }
+
+            public void DisplayRemainingTimeUntilDeparture()
+            {
+                TimeSpan remainingTime = DepartureTime - DateTime.Now;
+                Console.WriteLine($"Оставшееся время до вылета: {remainingTime.Days} дней, {remainingTime.Hours} часов, {remainingTime.Minutes} минут");
+            }
+        }
+    }
+}
